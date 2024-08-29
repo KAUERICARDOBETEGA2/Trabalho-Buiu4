6665854b96bf5d0ec021fbb104fd3470c7a881e5
KAUERICARDOBETEGA2 <165172082+KAUERICARDOBETEGA2@users.noreply.github.com>
2024-08-29 17:24:55 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+export const perguntas = [
+    {
+        enunciado: "Você acorda na sua cama fria com um pingo de água caindo em sua testa e os sinos das irmãs soando em seus ouvidos. Seu quarto mais se assemelha a uma prisão de segurança máxima - Sério, quem em sã consciência teve a ideia desse quarto? -, além das paredes úmidas, do silêncio gritante e da coleira de metal apertada em seu pescoço, você tem pouco tempo para sair de seu quarto/cela. Com uma batida na porta, você rapidamente ajeita seus curtos cabelos brancos e se levanta para sair. Você chega no refeitório do local, que fica em frente a um portão de grade eletrificada - A entrada -, onde diariamente crianças entram. Enquanto você comia, percebeu que todos estavam muito quietos e teve uma ideia.",
+        alternativas: [
+            {
+                texto: "A) Começar uma guerra de comida para animar o dia. Não é sempre que a gente se diverte né?",
+                afirmacao: [
+                    " Você pega o seu mingau de arroz e taca na criança a sua frente. Ela taca em outra criança e assim começa uma guerra de comida brutal. Rindo, você se esconde embaixo da mesa do refeitório e assiste ao caos que você criou. Quando você vai pegar mais comida para tacar, percebe que uma das irmãs pegou você pelo braço e te deixou em confinamento no seu quarto sem nem olhar no seu rosto, como se tivesse nojo."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Desistir da ideia. Qualquer coisa que eu fizer vai acarretar mais problemas, e provavelmente vão me punir.",
+                afirmacao: [
+                    " Você termina de comer seu lanche e enquanto você olha em volta percebe o triste rosto das crianças o tédio nunca foi tão chato quanto hoje. Você sente que não consegue se segurar de fazer algo e solta um longo suspiro. Alguns minutos depois você volta ao seu quarto/cela."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Depois de muito tempo olhando para o teto de seu quarto/cela tedioso, você vê que um pedaço da parede caiu e começa a rabiscar as paredes e brincar como se fosse uma bola. Ainda assim é muito tediante. Depois de longos 10 minutos brincando com aquele pedaço de cimento, você percebe que um papel com um tipo de giz de cera meio quebrado em cima foi passado pelo vão da parede. Você pega e lê o que tá escrito. _Olá, me chamo Kyoko. Eu cheguei faz pelo menos uns 30 minutos e aqui é... estranhamente silencioso para um internato infantil. Você pode conversar comigo nesse meio tempo?_ depois de ler o pequeno texto com uma ótima calografia do grande papel, você sente até vergonha de responder com seus garranchos, mas está tão animada que não consegue deixar essa chance passar. Vocês conversam o dia inteiro, e ainda assim, não fica claro se a pessoa do outro lado da parede é um garoto ou uma garota - Porque nomes japoneses tem que ser tão neutros? - e mesmo com toda essa curiosidade você nao pergunta o gênero por ter medo de ser desrespeitoso. Depois de muito tempo conversando com ela, ela te manda um ultimo bilhete de boa noite com uma observação escrita. _Eu tirei 60 naquela prova rosa. Devo me preocupar?_ Você responde que não faz ideia e se deita na sua cama para uma boa noite de sono.",
+        alternativas: [
+            {
+                texto: "A) Contar a verdade. Mesmo não sendo importante futuramente nem agora, é bom contar as coisas pros amigos, certo?",
+                afirmacao: [
+                    "Você pega novamente o bilhete e explica o porque da prova rosa. A prova rosa é uma prova para testar seus conhecimentos gerais em qualquer assunto como qualquer prova comum. Seu diferencial, é que se você tira uma nota acima de 80 eles te levam para a sala rosa e nunca mais vêem a criança. É de certo jeito até assustador. Você dorme com certo receio, mas confia em seu novo amigo.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B) Se manter em silêncio. Não é importante de nenhum jeito, e se alguma irmã descobrir que eu sei, eu vou estar em grandes problemas.",
+                afirmacao: [
+                    "Você dorme tranquilamente."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+];
